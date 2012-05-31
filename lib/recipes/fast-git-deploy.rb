fd6f8dc0695880ac2de9f35764f08f3246462215
Nicola Racco <nicola@nicolaracco.com>
2012-05-31 11:39:06 +0200

    set the current release to the current path

--- lib/recipes/fast-git-deploy.rb
+++ lib/recipes/fast-git-deploy.rb
@@ -3,6 +3,7 @@ set :scm, :git
 set :logged_user, Cap::Git::Deploy.current_user
 set :branch, ENV['branch'] || Cap::Git::Deploy.current_branch unless exists? :branch
 set(:latest_release) { fetch :current_path }
+set(:current_release) { fetch :current_path }
 
 namespace :deploy do
   desc "Setup a GitHub-style deployment"
